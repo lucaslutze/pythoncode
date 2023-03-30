771e6a11adf2a7845389b43f3ca86b8326285187
Lucas Lutze <luclut060627@student.jenseneducation.se>
2023-03-30 09:11:51 +0200

    a good start on project

--- To-do List.py
+++ To-do List.py
@@ -1,1 +1,34 @@
-print("Welcome to your to-do list! Select existing to-do list below or create a new one!")
+import os
+
+folderPath = "C:/Users/LucasLutze/PycharmProjects/pythonProject1"
+files = os.listdir(folderPath)
+txtFiles = [f for f in files if f.endswith('.txt')]
+
+print("Welcome to your to-do list! Select existing to-do list below or create a new one:")
+
+for txtFiles in txtFiles:
+    print(txtFiles)
+
+fileName = input("Please enter the name of the file you want to edit or create: ")
+filePath = os.path.join(folderPath, fileName)
+
+fileCreate = ""
+
+if os.path.exists(filePath):
+    file = open(filePath, "a")
+else:
+    open(fileName, "a").close
+
+if fileCreate == "yes":
+    file.write(input("Please enter your tasks here: "))
+
+else:
+    fileCreate = input("The list could not be located. Would you like to create one? (yes/no) ").lower()
+
+if fileCreate == "yes":
+    print("Please enter your tasks: ")
+    file = open(filePath, "w")
+elif fileCreate == "no":
+    print("Okay, try to search for your file again")
+else:
+    print("Unable to understand prompt! ")
